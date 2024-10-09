4ca6805722afc9bcb865f9484acc736204b5a01f
João Pedro <joaope.espindola2005@gmail.com>
2024-10-09 15:36:24 -0300

    Connecting to the database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require ("express-async-errors")
+const database = require("./database/sqlite")
 const AppError = require("./utils/AppError")
 const express = require("express")
 const cors = require("cors")
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+database()
 
 app.use(( error, request, response, next) => {
  if (error instanceof AppError){
