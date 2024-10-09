c7cc66aff96e984ba9f88b909e5bad2da502e374
João Pedro <joaope.espindola2005@gmail.com>
2024-10-09 14:27:13 -0300

    Configure express.json() so that your application knows how to deal with json data;

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
+app.use(express.json())
+
+app.post("/users", (request, response) => {
+ const {name, email, password} = request.body
+ 
+
+ response.json({ name, email, password })
+})
 
 const PORT = 3333 
 app.listen(PORT, () => console.log(`Server is Running on Port ${PORT}`))
