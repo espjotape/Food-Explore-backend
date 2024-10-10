07ecfac4c8384cf2dbc91a3ecf76efec56087db2
João Pedro <joaope.espindola2005@gmail.com>
2024-10-10 19:48:59 -0300

    authentication route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const dishesRouter = require("./dishes.routes")
+const sessionRoutes = require("./sessions.routes")
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/dishes', dishesRouter)
-
+routes.use('/sessions', sessionRoutes)
 
 module.exports = routes
