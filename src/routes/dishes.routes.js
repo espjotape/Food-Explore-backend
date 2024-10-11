3b5f3f7d7f242550dfc5c15dc409934fca629bc7
João Pedro <joaope.espindola2005@gmail.com>
2024-10-11 10:57:11 -0300

    Loading Images

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,4 +1,6 @@
-const { Router } = require ('express')
+const { Router, response } = require ('express')
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const DishesController = require ("../controllers/DishesController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
@@ -6,11 +8,15 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const dishesController = new DishesController()
 
 const dishesRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 dishesRoutes.get("/", dishesController.index)
 dishesRoutes.post("/", dishesController.create)
 dishesRoutes.get("/:id", dishesController.show)
 dishesRoutes.delete("/:id", dishesController.delete)
 dishesRoutes.put("/:id", ensureAuthenticated ,dishesController.update)
+dishesRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, response) => {
+ console.log(req.file.filename)
+})
 
 module.exports = dishesRoutes
