89ac1016fa8da878ef417c8eed882cfeddee4e3d
João Pedro <joaope.espindola2005@gmail.com>
2024-10-31 14:23:54 -0300

    Add routes for orders and ordersItems

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const usersRouter = require("./users.routes")
 const dishesRouter = require("./dishes.routes")
 const sessionRoutes = require("./sessions.routes")
 const favoritesRoutes = require("./favorites.routes")
+const ordersRoutes = require("./orders.routes")
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/orders',ordersRoutes)
 routes.use('/dishes', dishesRouter)
 routes.use('/sessions', sessionRoutes)
 routes.use('/favorites', favoritesRoutes)
